d83a40616d2ecaf1a9d868da3b2ab84c70db109a
bcornw2 <bcornw2@students.towson.edu>
2018-02-13 18:01:12 -0500

    task 11 - etracted interface with setOwnable and GetOwnable methods

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -1,13 +1,22 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
-public abstract class Cell {
+public abstract class Cell implements IOwnable {
+	private boolean available = true;
 	private String name;
 	protected Player theOwner;
 
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc442.project1.monopoly.IOwnable#getName()
+	 */
+	@Override
 	public String getName() {
 		return name;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc442.project1.monopoly.IOwnable#getTheOwner()
+	 */
+	@Override
 	public Player getTheOwner() {
 		return theOwner;
 	}
@@ -16,12 +25,24 @@ public abstract class Cell {
 		return 0;
 	}
 
+	public boolean isAvailable() {
+		return available;
+	}
+	
 	public abstract void playAction();
 
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	
 	void setName(String name) {
 		this.name = name;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc442.project1.monopoly.IOwnable#setTheOwner(edu.towson.cis.cosc442.project1.monopoly.Player)
+	 */
+	@Override
 	public void setTheOwner(Player owner) {
 		this.theOwner = owner;
 	}
@@ -29,9 +50,4 @@ public abstract class Cell {
     public String toString() {
         return name;
     }
-
-	public boolean isAvailable() {
-		// TODO Auto-generated method stub
-		return false;
-	}
 }
---
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/IOwnable.java
@@ -0,0 +1,11 @@
+package edu.towson.cis.cosc442.project1.monopoly;
+
+public interface IOwnable {
+
+	String getName();
+
+	Player getTheOwner();
+
+	void setTheOwner(Player owner);
+
+}
